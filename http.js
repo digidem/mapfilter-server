8da1eb64cd2ceccdc36cbca4c2596035b0f72fdc
karissa <krmckelv@gmail.com>
2018-02-27 18:35:52 -0500

    cleanup, remove links http function for now

--- http.js
+++ http.js
@@ -4,19 +4,6 @@ var xtend = require('xtend')
 var router = require('routes')()
 var pump = require('pump')
 
-router.addRoute('GET /obs/links/:id', function (req, res, m) {
-  pump(m.db.obs.links(m.params.id), through.obj(write), res, done)
-  function write (row, enc, next) {
-    next(null, JSON.stringify(row) + '\n')
-  }
-  function done (err) {
-    if (err) {
-      res.statusCode = 500
-      res.end(err + '\n')
-    }
-  }
-})
-
 router.addRoute('GET /obs/list', function (req, res, m) {
   pump(m.db.observationStream(), ndjson.stringify(), res, done)
   function done (err) {
